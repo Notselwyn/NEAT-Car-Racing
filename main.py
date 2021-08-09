ba2c5798f0e2e40588a75955a91e8253e67f5cbc
Lau <68616630+Notselwyn@users.noreply.github.com>
2021-08-09 19:14:29 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import neat
 import numpy as np
-import os
 import pickle
-import pygame
 from functools import cache
 from PIL import Image
-from scipy import ndimage
-from skimage.transform import resize
+from numba import jit
 
 """" 
              ██████╗  ██████╗  ███╗   ██╗ ███████╗ ██╗  ██████╗  
@@ -16,20 +13,31 @@ from skimage.transform import resize
             ╚██████╗ ╚██████╔╝ ██║ ╚████║ ██║      ██║ ╚██████╔╝ 
              ╚═════╝  ╚═════╝  ╚═╝  ╚═══╝ ╚═╝      ╚═╝  ╚═════╝  
 """
-# Amount of generations.
-amount_generations = 300
-
-# Exit on first finish-line. This prevents an infinite loop where if the model is trained perfectly, it will keep running.
-exit_finish_line = True
-
 # Path to the track image. For making custom tracks, check the GitHub readme.
-path_track = "Files/track-1.png"
+path_track = "Files/track-3.png"
+
+# Speed of the car in pixels
+speed = 5
+
+# Angle in degrees in which the car can turn in 1 move
+# Intensity = 20 gives -20 to 20 degrees in turns.
+angle_intensity = 15
+
+# The start angle of the car
+# You should change this accordingly when customizing the track
+car_angle = 90
+
+# Finish line color [R, G, B]
+flc = [255, 0, 0]
+
+# Road color [R, G, B]
+rc = [64, 64, 64]
 
 # Path to the car image.
 path_car = "Files/car.png"
 
 # Path to the .pkl file in which the AI will be saved and loaded.
-path_ai_file = "Files/winner-track-1.pkl"
+path_ai_file = "Files/winner-track-3.pkl"
 
 # Replay the genome specified in path_ai_file.
 replay_genome = False
@@ -42,9 +50,20 @@ fps = 144
 # This is where you will find the Neural Network settings such as population size and biases.
 path_feedforward = "Files/config-feedforward.txt"
 
-# This will toggle the NEAT stats: fitness, id, size, adj fit, et cetera.
-neat_statistics = True
+# Max amount of generations.
+amount_generations = 5000
 
+# Exit on first finish-line. This prevents an infinite loop where if the model is trained perfectly, it will keep running.
+exit_finish_line = False
+
+# This will toggle the NEAT stats both in the console
+neat_statistics = False
+
+# Toggle the pygame GUI
+pygame_toggle = True
+
+# Show debug statistics and overlay the screen
+debug = False
 
 """"
              ██████╗  ██████╗  ███████╗  ███████╗ 
@@ -54,58 +73,63 @@ neat_statistics = True
             ╚██████╗ ╚██████╔╝ ██████╔═╝ ███████╗ 
              ╚═════╝  ╚═════╝  ╚═════╝   ╚══════╝ 
 """
-pygame.init()
-clock = pygame.time.Clock()
-size = [600, 450]  # Size of the PyGame window
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption('Car Racing: NEAT')  # Name of the PyGame window
+if __name__ == "__main__":
+    track_array_visual = np.array(Image.open(path_track).convert(colors=5))  # Process track image into Numpy array
 
-font = pygame.font.Font('freesansbold.ttf', 20)  # Font used for the text displayed
-white = (255, 255, 255)
+    track_array = np.array([[int(np.average(y)) for y in x] for x in track_array_visual])
+    start_car_coords = np.where(track_array == int(np.average(flc)))
+    car_coords_x = round(np.average(start_car_coords[1]))
+    car_coords_y = round(np.average(start_car_coords[0]))
+    track_array = np.vectorize(lambda x: 0 if x != np.average(rc) and x != np.average(flc) else 1)(track_array)
+    car_coords = (car_coords_y, car_coords_x)
 
-track = pygame.image.load(os.path.join(path_track))  # Load the track image
-track_array = np.array(Image.open(os.path.join(path_track)).convert(colors=5))  # Process track image into Numpy array
+    best_fit = 0
+    generation = 0
 
-best_fit = 0
-generation = 0
+    if pygame_toggle:
+        import pygame
 
-# Lists that can be used for diagnostics
-avg_performance_list = []
-best_performance_list = []
-genome_count_list = []
+        pygame.init()
+        clock = pygame.time.Clock()
+        pygame.display.set_caption('Car Racing: NEAT')  # Name of the PyGame window
 
-# Default variables for the car
-car = pygame.image.load(os.path.join(path_car))  # Load car image
-car_coords_x = round(np.average(np.where(track_array == [255, 0, 0])[1]))
-car_coords_y = round(np.average(np.where(track_array == [255, 0, 0])[0]))
-car_coords = (car_coords_x, car_coords_y)  # Get average x, y location of every red pixel. This is used as a finish- and start line
-car_angle = 90
-car_rect = car.get_rect()
-car_rect.center = (car_coords[0] + car.get_width(), car_coords[1])
+        track = pygame.image.load(path_track)  # Load the track image
+        font = pygame.font.Font('freesansbold.ttf', 20)  # Font used for the text displayed
+        size = [len(track_array[0]), len(track_array)]  # Size of the PyGame window
+        screen = pygame.display.set_mode(size)
 
-# Required for getting the location of the text. The non-rect variable doesn't have a .width property
-# This is only required for the right-sided text, because the left text is fixed.
-text_genome_generation = font.render(f"Generation: 0", True, white)
-text_genome_generation_rect = text_genome_generation.get_rect()
-text_genome_generation_rect.topleft = (size[0]-text_genome_generation_rect.width, 0)
+        # Default variables for the car
+        car = pygame.image.load(path_car)  # Load car image
+        car_rect = car.get_rect()
+        car_rect.center = (car_coords[0] + car.get_width(), car_coords[1])
 
-text_genome_best = font.render(f"Best Fit: 0", True, white)
-text_genome_best_rect = text_genome_best.get_rect()
-text_genome_best_rect.topleft = (size[0]-text_genome_best_rect.width, 20)
+
+        # Required for getting the location of the text. The non-rect variable doesn't have a .width property
+        # This is only required for the right-sided text, because the left text is fixed.
+        text_genome_generation = font.render(f"Generation: 0", True, (255, 255, 255))
+        text_genome_generation_rect = text_genome_generation.get_rect()
+        text_genome_generation_rect.topleft = (size[0] - text_genome_generation_rect.width, 0)
+
+        text_genome_best = font.render(f"Best Fit: 0", True, (255, 255, 255))
+        text_genome_best_rect = text_genome_best.get_rect()
+        text_genome_best_rect.topleft = (size[0] - text_genome_best_rect.width, 20)
+
+    if neat_statistics and not replay_genome:
+        from requests import Session
+        from multiprocessing import Process
+        from stats import neat_stats
+
+        sess = Session()
+        stats_process = Process(target=neat_stats)
+        stats_process.start()
 
 
 def replay_genome_func():
-    # Load requried NEAT config
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, path_feedforward)
-
-    # Unpickle saved winner
     with open(path_ai_file, "rb") as f:
         genome = pickle.load(f)
 
-    # Convert loaded genome into required data structure
     genomes = [(1, genome)]
-
-    # Call game with only the loaded genome
     main(genomes, config)
 
 
@@ -118,69 +142,65 @@ def run():
     - Et cetera
     """
 
-    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, os.path.join(path_feedforward))
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, path_feedforward)
 
+    p = neat.Population(config)
     if neat_statistics:
-        p = neat.Population(config)
         p.add_reporter(neat.StdOutReporter(True))
         p.add_reporter(neat.StatisticsReporter())
-
-    winner = p.run(main, amount_generations)
+    try:
+        winner = p.run(main, amount_generations)
+    except Exception as e:
+        if neat_statistics:
+            stats_process.terminate()
+        raise e
 
     # Save the winner object
     with open(path_ai_file, "wb") as f:
         pickle.dump(winner, f)
-
-    print(best_performance_list)
-    print(avg_performance_list)
-    print(genome_count_list)
+    print("Hit threshold maximum.")
+    if neat_statistics:
+        stats_process.terminate()
 
 
 @cache
-def rotate_array(angle: int, coords: tuple) -> np.array:
-    """" Rotates a Numpy array, but needs coordinates to get the area.
-    The ladder is due to limitations of @cache, which speeds up the code A LOT, so it's worth the trouble.
+@jit(nopython=True)
+def map_area(angle: int, coords: tuple) -> np.array:
+    """" Basically gets the coordinates in a 180 degree circle in front of the car
 
     :param angle: The angle of the car/genome.
     :param coords: The coordinates of the car/genome.
     """
 
-    tenbyten_area = tuple([round(np.average(y)) for y in x] for x in track_array[coords[1] - 10:coords[1] + 10, coords[0] - 10:coords[0] + 10])
-    arr = ndimage.rotate(tenbyten_area, angle, axes=(1, 0), mode='nearest')
-    arr[arr <= 53] = 0
-    arr[arr > 53] = 1
-    arr = np.array([[int(str(x)[0]) for x in y] for y in resize(arr, (10, 10))])
-    arr[arr <= 3] = 0
-    arr[arr > 3] = 1
-    return arr
+    return [track_array[new_coordinates(coords, (angle+x) % 360)] for x in range(-90, 91, 5)]
 
 
-@cache
-def new_coordinates(coordinates: tuple, angle: int, speed: int) -> (int, int):
-    """" Calculates the new coordinates based on the speed, angle and current.
+@jit(nopython=True)
+def new_coordinates(coords: tuple, angle: int) -> (int, int):
+    """" Calculates the new coordinates based on the speed, angle and current coordiantes.
 
-    :param coordinates: The coordinates of the car/genome.
+    :param coords: The coordinates of the car/genome.
     :param angle: The angle of the car/genome.
-    :param speed: The speed of all cars/genomes.
     """
 
     raw_coords_x = angle / 180 - 1
     if raw_coords_x > 0.5:
-        raw_coords_x = 2 - angle/180
+        raw_coords_x = 2 - angle / 180
     elif -0.5 > raw_coords_x:
-        raw_coords_x = -angle/180
+        raw_coords_x = -angle / 180
 
     coords_x = speed * raw_coords_x * 2
     coords_y = speed - abs(coords_x)
-
     if not 270 > angle > 90:
         coords_y = -coords_y
 
-    # This code might come in handy in the future. This calculates the difference between new and old coords.
-    """if increasements:
-        return round(coords_x), round(coords_y)
-    else:"""
-    return round(coordinates[0] + coords_x), round(coordinates[1] + coords_y)
+    return int(coords[0] + coords_y), int(coords[1] + coords_x)
+
+
+@cache
+@jit(nopython=True)
+def diff(x: int, y: int) -> int:
+    return (((x + 180) - (y + 180)) + 180) % 360 - 180
 
 
 def main(genomes: list, config):
@@ -190,107 +210,102 @@ def main(genomes: list, config):
     :param config: The NEAT config, basically Neural Network settings.
     """
 
-    global best_fit, avg_performance_list, best_performance_list, generation
+    global best_fit, generation
 
     net_array = {}
     coords_array = {}
     angle_array = {}
-
+    angle_history = {}
     best_fit_gen = 0
-    speed = 5
     generation += 1
 
     for genome_id, genome in genomes:
         net_array[genome_id] = neat.nn.FeedForwardNetwork.create(genome, config)
         angle_array[genome_id] = car_angle
+        angle_history[genome_id] = (0, 0, 0, 0, 0, 0, car_angle)
         coords_array[genome_id] = car_coords
         genomes[genomes.index((genome_id, genome))][1].fitness = 0
 
     while len(coords_array) > 0:
-        screen.fill([255, 255, 255])
-        screen.blit(track, (0, 0))
-        text_genome_count = font.render(f"Genomes: {len(coords_array)}", True, white)  # Make a text
-        text_genome_fitness = font.render(f"Avg Fit: {round(np.average([x[1].fitness for x in genomes]), 2)}", True, white)
-        text_genome_gen_best = font.render(f"Best Fit Gen: {best_fit_gen}", True, white)
+        if pygame_toggle:
+            screen.blit(track, (0, 0))
+            if debug:
+                surf = pygame.surfarray.make_surface(np.rot90(np.fliplr(track_array)))
+                surf.set_alpha(120)
+                screen.blit(surf, (0, 0))
 
-        text_genome_generation = font.render(f"Generation: {generation}", True, white)
-        text_genome_generation_rect = text_genome_generation.get_rect()  # Get the rect of the text
-        text_genome_generation_rect.topleft = (size[0]-text_genome_generation_rect.width, 0)  # Center the rect around the middle
+            if neat_statistics:
+                text_genome_count = font.render(f"Genomes: {len(coords_array)}", True, (255, 255, 255))  # Make a text
+                text_genome_fitness = font.render(f"Avg Fit: {round(np.average([x[1].fitness for x in genomes]), 2)}", True, (255, 255, 255))
+                text_genome_gen_best = font.render(f"Best Fit Gen: {best_fit_gen}", True, (255, 255, 255))
 
-        text_genome_best = font.render(f"Best Fit: {best_fit}", True, white)
-        text_genome_best_rect = text_genome_best.get_rect()  # Get the rect of the text
-        text_genome_best_rect.topleft = (size[0]-text_genome_best_rect.width, 20)  # Center the rect around the middle
+                text_genome_generation = font.render(f"Generation: {generation}", True, (255, 255, 255))
+                text_genome_generation_rect = text_genome_generation.get_rect()  # Get the rect of the text
+                text_genome_generation_rect.topleft = (size[0] - text_genome_generation_rect.width, 0)  # Center the rect around the middle
 
-        screen.blit(text_genome_count, (0, 0))
-        screen.blit(text_genome_fitness, (0, 20))
-        screen.blit(text_genome_gen_best, (0, 40))
-        screen.blit(text_genome_generation, text_genome_generation_rect)
-        screen.blit(text_genome_best, text_genome_best_rect)
+                text_genome_best = font.render(f"Best Fit: {best_fit}", True, (255, 255, 255))
+                text_genome_best_rect = text_genome_best.get_rect()  # Get the rect of the text
+                text_genome_best_rect.topleft = (size[0] - text_genome_best_rect.width, 20)  # Center the rect around the middle
         for genome_id, genome in genomes:
             if genome_id in coords_array:
                 coords = coords_array[genome_id]
                 angle = angle_array[genome_id]
-
-                increasement_dir = round(net_array[genome_id].activate(rotate_array(angle, coords).flat)[0])
-
-                if increasement_dir == 1:
-                    angle += 10
-                elif increasement_dir == -1:
-                    angle -= 10
-
-                if track_array[coords[1]][coords[0]].tolist() == [0, 127, 14]:
+                if track_array[coords] == 0 or diff(angle_history[genome_id][0], angle) > 90:
                     del coords_array[genome_id]
                     continue
 
-                if exit_finish_line and track_array[coords[1]][coords[0]].tolist() == [255, 0, 0] and genome.fitness > 50:
+                if exit_finish_line and track_array_visual[coords].tolist() == flc and genome.fitness > 20:
                     if not replay_genome:
-                        # Save the winner object
                         with open(path_ai_file, "wb") as f:
                             pickle.dump(genome, f)
 
-                        print(best_performance_list)
-                        print(avg_performance_list)
-                        print(genome_count_list)
-
+                        print("Reached the finish line.")
+                        if neat_statistics:
+                            stats_process.terminate()
                         exit()
                     else:
                         replay_genome_func()
 
-
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        exit()
-
-                if angle >= 360:
-                    angle = 0
-                elif angle < 0:
-                    angle = 359
-
+                angle = (angle + net_array[genome_id].activate(map_area(angle, coords))[0] * angle_intensity) % 360
+                coords_array[genome_id] = new_coordinates(coords, angle)
+                angle_array[genome_id] = angle
+                angle_history[genome_id] = (*angle_history[genome_id][1:], angle)
                 genomes[genomes.index((genome_id, genome))][1].fitness += 1
 
-                coords = new_coordinates(coords, angle, speed)
-                coords_array[genome_id] = coords
+                if pygame_toggle:
+                    spoof_car = pygame.transform.rotate(car, angle - 90)
+                    spoof_car_rect = spoof_car.get_rect()
+                    spoof_car_rect.center = coords_array[genome_id][::-1]
+                    screen.blit(spoof_car, spoof_car_rect)
 
-                spoof_car = pygame.transform.rotate(car, angle - 90)
-                spoof_car_rect = spoof_car.get_rect()
-                spoof_car_rect.center = (coords[0], coords[1])
-                angle_array[genome_id] = angle
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            exit()
+        bfg = max(genomes, key=lambda x: x[1].fitness)[1]
+        best_fit = max(bfg.fitness, best_fit)
+        best_fit_gen = bfg.fitness
+        if not replay_genome:
+            with open(path_ai_file + "-unfinished", "wb") as f:
+                pickle.dump(max(genomes, key=lambda x: x[1].fitness)[1], f)
 
-                screen.blit(spoof_car, spoof_car_rect)
+        if pygame_toggle:
+            if neat_statistics and not replay_genome:
+                screen.blit(text_genome_count, (0, 0))
+                screen.blit(text_genome_fitness, (0, 20))
+                screen.blit(text_genome_gen_best, (0, 40))
+                screen.blit(text_genome_generation, text_genome_generation_rect)
+                screen.blit(text_genome_best, text_genome_best_rect)
 
-        for _, genome in genomes:
-            if genome.fitness > best_fit:
-                best_fit = genome.fitness
+            pygame.display.flip()
 
-            if genome.fitness > best_fit_gen:
-                best_fit_gen = genome.fitness
+            if replay_genome:
+                clock.tick(fps / 2)
 
-        pygame.display.flip()
-        if replay_genome is True:
-            clock.tick(fps / 2)
-    best_performance_list.append(best_fit)
-    avg_performance_list.append(round(np.average([x[1].fitness for x in genomes]), 2))
-    genome_count_list.append(len(genomes))
+    if neat_statistics:
+        try:
+            sess.post("http://127.0.0.1:1234", json={"bf": best_fit, "af": round(np.average([x[1].fitness for x in genomes]), 2), "gc": len(genomes), "gbf": best_fit_gen, "gen": generation}, timeout=1)
+        except Exception:
+            print("Successfully sent data to stats server")
 
 
 if __name__ == '__main__':
---
+++ stats.py
@@ -0,0 +1,89 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import matplotlib.pyplot as plt
+
+global avg_performance_list
+global best_performance_list
+global genome_count_list
+global gen_performance_list
+global manager
+
+
+class Graph:
+    def __init__(self, title, ylabel):
+        self.figure, self.ax = plt.subplots()
+        self.lines, = self.ax.plot([], [])
+        self.ax.set_xlabel("Generation")
+        self.ax.set_ylabel(ylabel)
+        self.ax.set_title(title)
+        self.ax.grid()
+        self.update([], 0)
+
+
+    def update(self, arr, gen):
+        self.lines.set_xdata(range(int(gen)))
+        self.lines.set_ydata(arr)
+        if len(arr) != 0:
+            self.ax.set_xlim(0, int(gen))
+            self.ax.set_ylim(0, int(max(arr)*1.10))
+        self.ax.relim()
+        self.figure.canvas.draw()
+        self.figure.canvas.flush_events()
+
+
+class Manager:
+    def __init__(self, labels: list[tuple]):
+        plt.ion()
+        self.graphs = []
+        for ylabel, title in labels:
+            self.graphs.append(Graph(title, ylabel))
+
+    def update(self, data: list[tuple]):
+        for pos, (data_arr, gen) in enumerate(data):
+            self.graphs[pos].update(data_arr, gen)
+
+
+class MessageHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        global avg_performance_list
+        global best_performance_list
+        global genome_count_list
+        global gen_performance_list
+        global manager
+
+        self.send_response(200)
+        length = int(self.headers.get('Content-length', 0))
+        data = json.loads(self.rfile.read(length).decode())
+        avg_performance_list += [data["af"]]
+        best_performance_list += [data["bf"]]
+        genome_count_list += [data["gc"]]
+        gen_performance_list += [data["gbf"]]
+        generation = data["gen"]
+        manager.update(((genome_count_list, generation),
+                        (gen_performance_list, generation),
+                        (best_performance_list, generation),
+                        (avg_performance_list, generation)))
+
+        print(f"Added data: {data}")
+
+
+def neat_stats():
+    global avg_performance_list
+    global best_performance_list
+    global genome_count_list
+    global gen_performance_list
+    global manager
+
+    best_performance_list = []
+    genome_count_list = []
+    gen_performance_list = []
+    avg_performance_list = []
+    manager = Manager((("Genomes", "Genome Count"),
+                       ("Fitness", "Best Fitness Per Generation"),
+                       ("Fitness", "Best Fitness All Time"),
+                       ("Fitness", "Average Fitness Per Generation")))
+
+    server_address = ("127.0.0.1", 1234)
+    httpd = HTTPServer(server_address, MessageHandler)
+    print("Booted stats listener: waiting for requests")
+    httpd.serve_forever()
